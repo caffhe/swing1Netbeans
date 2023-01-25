d5ffb62e772710d6027937afd4abfe66c5ec1f53
Herbert Caffarel <herbert.caffarel@cicef.pro>
2023-01-25 11:30:45 +0100

    First commit

---
+++ src/main/java/fr/ldnr/herbert/swing1netbeans/Swing1Netbeans.java
@@ -0,0 +1,15 @@
+/*
+ */
+
+package fr.ldnr.herbert.swing1netbeans;
+
+/**
+ *
+ * @author Herbert Caffarel
+ */
+public class Swing1Netbeans {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
