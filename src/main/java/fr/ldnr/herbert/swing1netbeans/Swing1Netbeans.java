58494253ba03101f7f80657efd9bfbcfe9f52816
Herbert Caffarel <herbert.caffarel@cicef.pro>
2023-01-25 11:55:13 +0100

    Show configurated window

--- src/main/java/fr/ldnr/herbert/swing1netbeans/Swing1Netbeans.java
+++ src/main/java/fr/ldnr/herbert/swing1netbeans/Swing1Netbeans.java
@@ -3,6 +3,8 @@
 
 package fr.ldnr.herbert.swing1netbeans;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author Herbert Caffarel
@@ -10,6 +12,10 @@ package fr.ldnr.herbert.swing1netbeans;
 public class Swing1Netbeans {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        JFrame myWindow = new JFrame();
+
+        myWindow.setSize(200, 200);
+        myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myWindow.setVisible(true);
     }
 }
