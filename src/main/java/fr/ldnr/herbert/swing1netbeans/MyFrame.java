d7cf92da712e61bccb27bd65503214d22e6cd579
Herbert Caffarel <herbert.caffarel@cicef.pro>
2023-01-26 08:11:19 +0100

    Using extended JFrame class

---
+++ src/main/java/fr/ldnr/herbert/swing1netbeans/MyFrame.java
@@ -0,0 +1,40 @@
+package fr.ldnr.herbert.swing1netbeans;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Herbert Caffarel
+ */
+public class MyFrame extends JFrame {
+
+    private int width;
+    private int height;
+
+    public MyFrame() {
+        this(200, 200);
+    }
+
+    public MyFrame(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.setUpAndDisplay();
+    }
+
+    private void setUpAndDisplay() {
+        JPanel content = new JPanel();
+        JButton jb1 = new JButton("Click me!");
+        jb1.addActionListener((e) -> {
+            System.out.println("Coucou");
+        });
+        content.add(jb1);
+        this.setContentPane(content);
+
+        this.setSize(this.width, this.height);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+}
--- src/main/java/fr/ldnr/herbert/swing1netbeans/Swing1Netbeans.java
+++ src/main/java/fr/ldnr/herbert/swing1netbeans/Swing1Netbeans.java
@@ -3,7 +3,6 @@
 
 package fr.ldnr.herbert.swing1netbeans;
 
-import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -15,30 +14,7 @@ import javax.swing.JPanel;
 public class Swing1Netbeans {
 
     public static void main(String[] args) {
-        // Créer un cadre
-        JFrame myWindow = new JFrame();
-        /* Ajouter un gestionnaire de placement au cadre (meilleure pratique)
-          1/ On crée un Panel (qui par défaut contient un layout manager de
-             type FlowLayout
-          2/ On ajoute le Panel au cadre
-         */
-        JPanel jp = new JPanel();
-        myWindow.add(jp); // On pourrait utiliser setContentPane
-        // Créer un bouton
-        JButton jb1 = new JButton("Click me!");
-        // Ajouter le bouton dans le panel
-        jp.add(jb1);
-
-        // Surveiller le bouton pour répondre à un événement
-        jb1.addActionListener((e) -> {
-            System.out.println("Coucou");
-        });
-
-        // Dimensionner le cadre
-        myWindow.setSize(200, 200);
-        // Faire en sorte que fermer le cadre finisse l'application
-        myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // Afficher le cadre
-        myWindow.setVisible(true);
+        MyFrame mf = new MyFrame();
+        MyFrame mfPersonnalized = new MyFrame(400, 800);
     }
 }
