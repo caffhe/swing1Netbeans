ea54e55999a938870ecc7de96ad1082a2f64fbd3
Herbert Caffarel <herbert.caffarel@cicef.pro>
2023-01-26 07:53:09 +0100

    Add button to frame

--- src/main/java/fr/ldnr/herbert/swing1netbeans/Swing1Netbeans.java
+++ src/main/java/fr/ldnr/herbert/swing1netbeans/Swing1Netbeans.java
@@ -3,6 +3,7 @@
 
 package fr.ldnr.herbert.swing1netbeans;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 /**
@@ -12,10 +13,18 @@ import javax.swing.JFrame;
 public class Swing1Netbeans {
 
     public static void main(String[] args) {
+        // Créer un cadre
         JFrame myWindow = new JFrame();
+        // Créer un bouton
+        JButton jb1 = new JButton("Click me!");
+        // Ajouter le bouton dans le cadre (mauvaise pratique !)
+        myWindow.add(jb1);
 
+        // Dimensionner le cadre
         myWindow.setSize(200, 200);
+        // Faire en sorte que fermer le cadre finisse l'application
         myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Afficher le cadre
         myWindow.setVisible(true);
     }
 }
