92e1f44cbe1af93eec8d9a04557512c7ef82ac72
Herbert Caffarel <herbert.caffarel@cicef.pro>
2023-01-26 08:00:51 +0100

    Using JPanel as contentPane

--- src/main/java/fr/ldnr/herbert/swing1netbeans/Swing1Netbeans.java
+++ src/main/java/fr/ldnr/herbert/swing1netbeans/Swing1Netbeans.java
@@ -6,6 +6,7 @@ package fr.ldnr.herbert.swing1netbeans;
 import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  *
@@ -16,14 +17,17 @@ public class Swing1Netbeans {
     public static void main(String[] args) {
         // Créer un cadre
         JFrame myWindow = new JFrame();
-        // Ajouter un gestionnaire de placement au cadre
-        // On récupère la toile de fond et on y applique un layout manager
-        // de type FlowLayout
-        myWindow.getContentPane().setLayout(new FlowLayout());
+        /* Ajouter un gestionnaire de placement au cadre (meilleure pratique)
+          1/ On crée un Panel (qui par défaut contient un layout manager de
+             type FlowLayout
+          2/ On ajoute le Panel au cadre
+         */
+        JPanel jp = new JPanel();
+        myWindow.add(jp); // On pourrait utiliser setContentPane
         // Créer un bouton
         JButton jb1 = new JButton("Click me!");
-        // Ajouter le bouton dans le cadre
-        myWindow.add(jb1);
+        // Ajouter le bouton dans le panel
+        jp.add(jb1);
 
         // Surveiller le bouton pour répondre à un événement
         jb1.addActionListener((e) -> {
